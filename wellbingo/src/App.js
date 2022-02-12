549e29fd364668e85e01922a0a26feb1834ad8bd
saya0118 <chemi0802@gmail.com>
2022-02-12 11:05:36 -0800

    organized file

---
+++ wellbingo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const cardImage = [
+    {"src": "/img/"}
+]
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>WellBingo</h1>
+            <button>New Game</button>
+        </div>
+    )
+}
+
+export default App;
