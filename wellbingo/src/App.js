c347954da9fdf50ff19942ce5424cf0a33fd1e11
saya0118 <chemi0802@gmail.com>
2022-03-06 23:58:44 -0800

    imported edit icon from mui

--- wellbingo/src/App.js
+++ wellbingo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Board from './components/Board';
 import './scss/App.scss'
+import EditIcon from '@mui/icons-material/Edit';
 
 const App = () => {
     return (
         <div className="App">
             <h1>WellBingo</h1>
+            <EditIcon/>
             <Board/>
         </div>
     )
