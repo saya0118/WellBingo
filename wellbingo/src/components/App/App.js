c84fff0255ba9d2f0f425c89e237c3eb0d9370a9
saya0118 <chemi0802@gmail.com>
2022-03-08 12:25:20 -0800

    created singnIn component and js file/ scss file of it

--- wellbingo/src/components/App/App.js
+++ wellbingo/src/components/App/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Router, Route} from "react-router-dom";
-import { Home } from '../Home/Home'
-import { EditPage } from '../EditPage/EditPage'
+import { SignIn } from '../SignIn/SignIn';
+import { Home } from '../Home/Home';
+import { EditPage } from '../EditPage/EditPage';
 import './App.scss';
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
         <div className="App">   
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home/>}/>
+                    <Route path="/" element={<SignIn/>}/>
+                    <Route path="/home" element={<Home/>}/>
                     <Route path="/edit-page" element={<EditPage/>}/>
                 </Routes>
             </BrowserRouter>
