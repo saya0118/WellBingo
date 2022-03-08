9621fdc1b78aaec9c4cbf76c0dbb7dfec4a9667f
saya0118 <chemi0802@gmail.com>
2022-03-07 23:20:22 -0800

    set Router in App.js

--- wellbingo/src/App.js
+++ wellbingo/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Routes, Router, Route, Link } from "react-router-dom";
 import Board from './components/Board';
+import { EditButton } from './components/EditIcon';
+import { EditPage } from './components/EditPage'
 import './scss/App.scss';
-import { EditButton } from './components/EditIcon'
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>WellBingo</h1>
-            <EditButton/>
-            <Board/>
-        </div>
+        <Router>
+            <Routes>
+                <div className="App">
+                    <h1>WellBingo</h1>
+                    <Route path="/">
+                        <EditButton/>
+                        <Board/>
+                    </Route>
+                    <Route path="/EditPage">
+                        <EditPage/>
+                    </Route>
+                </div>
+
+            </Routes>
+        </Router>
     )
 }
 
