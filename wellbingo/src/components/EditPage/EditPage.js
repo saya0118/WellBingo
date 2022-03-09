6dacd2fc87cc0ab194878cc41c9ae7b313092934
saya0118 <chemi0802@gmail.com>
2022-03-08 16:43:38 -0800

    organized constructure of edit page

--- wellbingo/src/components/EditPage/EditPage.js
+++ wellbingo/src/components/EditPage/EditPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './EditPage.scss';
 
 export const EditPage = () => {
     return (
         <div className="edit-page">
             <h1>WellBingo</h1>
-            
+            <div className="edit-container">
+                <div className="h2-box">
+                    <h2>Create your own daily cards</h2>
+                </div>
+                <div className="list-box">
+                    <ul>
+                        <li>lalala</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
