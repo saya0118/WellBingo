6d7ad964ea09482841a6335ded1520590e76dfa9
saya0118 <chemi0802@gmail.com>
2022-03-09 13:06:26 -0800

    created done button in edit page

--- wellbingo/src/components/EditPage/EditPage.js
+++ wellbingo/src/components/EditPage/EditPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./EditPage.scss";
+import { Link } from "react-router-dom";
 import { EditList } from "../EditList/EditList";
 
 export const EditPage = () => {
@@ -9,6 +10,9 @@ export const EditPage = () => {
       <div className="edit-container">
         <div className="h2-box">
           <h2>Change your own bingo cards</h2>
+          <Link to='/home'>
+            <button className="done-button">Done</button>
+          </Link>
         </div>
         <EditList />
       </div>
