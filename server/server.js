e693bdabee84e2f9b9e2ee900b5bb5ac1039b4ca
saya0118 <chemi0802@gmail.com>
2022-04-20 22:12:17 -0700

    added url of server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 const mongoURI = process.env.MONGODB_URI;
 
-require('./models/index');
+require("./models/index");
 
-mongoose.connect(mongoURI, {
+mongoose
+  .connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
-    console.log('Connected to the database.');
-}).catch(err => {
-    console.log('Cannot connect to the database.');
+  })
+  .then(() => {
+    console.log("Connected to the database.");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database.");
     process.exit();
-});
+  });
 
 const app = express();
 
@@ -24,12 +27,16 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const todo = require('./routes/todo-router');
-app.use('/api', todo);
+const todo = require("./routes/todo-router");
+app.use("/api", todo);
+
+app.get("/", (req, res) => {
+  res.send("whatup");
+});
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}.`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
